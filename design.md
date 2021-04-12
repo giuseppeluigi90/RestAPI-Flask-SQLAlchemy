adea4ff386024a02c17f4592829dc5f0bf3c7e54
Alfonso de la Guarda <alfonodg@gmail.com>
2021-04-12 10:21:20 -0500

    Initial Release

---
+++ design.md
@@ -0,0 +1,58 @@
+Panel de Control
+================
+
+
+Menús y Tablas
+--------------
+
+* Información del cliente
+  
+    Correo, nombre, clave, etc.    
+
+* Objetivos
+  
+    Nombres de las webs, comercios o URLs a extraer    
+
+* Scrapers
+  
+    URLs y su meta data (JSON)
+
+* Programación (a qué hora?)
+  
+    Asignación de horario para ejecución de scrapers
+
+* Registros
+
+    Información de ejecución de scraper: fecha y hora, scraper, cantidad de elementos
+    extraídos y peticiones (requests), también posibles errores
+
+* Facturas
+  
+    Documentos comerciales emitidos, listos para su descarga
+
+* Pagos
+  
+    Información de pagos y operaciones realizadas
+
+* Terminos de Servicio
+
+    Contrato de servicios
+
+
+Vista Principal
+---------------
+
+* Scrapers y Registros
+
+    Lista de scrapers, sus registros y un botón para ejecución inmediata de los mismos
+
+
+
+Consideraciones
+---------------
+
+* Debe manejar sesiones y autentificación
+
+* Comunicación segura mediante CORS
+
+* La tabla registros se actualiza directamente desde los microservicios, construir un hub simple que lo procese
---
+++ scraprix_cp.py
@@ -0,0 +1,107 @@
+"""
+sudo -u postgres createuser scx
+sudo -u postgres createdb scxcp
+sudo -u postgres psql
+postgres=# alter user scx with encrypted password 'y2K.scx';
+ALTER ROLE
+postgres=# grant all privileges on database scxcp to scx;
+"""
+from sqlalchemy import create_engine, ForeignKey, Column, \
+    Integer, String, Float, JSON, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+db_string = "postgresql://scx:y2K.scx@scx/scxcp"
+engine = create_engine(db_string, echo=True)
+Base = declarative_base()
+
+
+class Account(Base):
+    __tablename__ = 'account'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+
+class Objectives(Base):
+    __tablename__ = 'objectives'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    name = Column(String)
+    status = Column(Boolean)
+
+
+class Scrapers(Base):
+    __tablename__ = 'scrapers'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    objective_id = Column(Integer, ForeignKey('objectives.id'), nullable=False)
+    target = Column(String)
+    params = Column(JSON)
+    structure = Column(JSON)
+    scraper_rel = relationship("Objectives", back_populates="scrapers")
+    status = Column(Boolean)
+
+
+class ScrapersTasks(Base):
+    __tablename__ = 'scrapers_tasks'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    scraper_id = Column(Integer, ForeignKey('scrapers.id'), nullable=False)
+    comment = Column(String)
+    task_at = Column(String)
+    scraper_task_rel = relationship("Scrapers", back_populates="scrapers_tasks")
+    status = Column(Boolean)
+
+
+class ScrapersLogs(Base):
+    __tablename__ = 'scrapers_logs'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    scraper_id = Column(Integer, ForeignKey('scrapers.id'), nullable=False)
+    register_at = Column(DateTime)
+    fragments = Column(Integer)
+    requests = Column(Integer)
+    messages = Column(String)
+    scraper_log_rel = relationship("Scrapers", back_populates="scrapers_tasks")
+    status = Column(Boolean)
+
+
+class Invoices(Base):
+    __tablename__ = 'invoices'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    invoice_serial = Column(String)
+    invoice_number = Column(Integer)
+    invoice_amount = Column(Float)
+    document_link = Column(String)
+    status = Column(Boolean)
+
+
+class Payments(Base):
+    __tablename__ = 'payments'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    messages_out = Column(JSON)
+    messages_in = Column(JSON)
+    status = Column(Boolean)
+
+
+# Account.invoices = relationship("Invoices", order_by = Invoices.id, back_populates = "account")
+Base.metadata.create_all(engine)
