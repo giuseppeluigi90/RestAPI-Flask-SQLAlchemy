35a7ad794d093cd60f5630fd6d910d8d2eebdcdb
Franky Cunza <franky.cunza@prix.tips>
2021-04-12 22:58:06 -0500

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -13,12 +13,13 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
 
     from .views import (
-        api, dashboard, register, scrapers
+        api, dashboard, register, scrapers, terms
     )
     app.register_blueprint(api.blueprint)
     app.register_blueprint(dashboard.blueprint)
     app.register_blueprint(register.blueprint)
     app.register_blueprint(scrapers.blueprint)
+    app.register_blueprint(terms.blueprint)
 
     # Processors
 
